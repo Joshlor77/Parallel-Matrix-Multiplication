2dccf1dd5aa5dc5ec2d46d1044c03ab48a2f7af3
Joshlor77 <joshlor77@gmail.com>
2025-08-02 20:58:20 -0500

    Forget about trying to make a locked deque

--- Test/ThreadPoolTest.cpp
+++ Test/ThreadPoolTest.cpp
@@ -1,19 +1,9 @@
 #include "../Implementation/ThreadPool.h"
 
-#include <iostream>
-#include <sstream>
 
-void print(){
-    std::ostringstream oss;
-    oss << "Work done by Thread ID: " << std::this_thread::get_id() << '\n';
-    std::cout << oss.str();
-}
 
 int main(){
-
     ThreadPool pool;
-    for (int i = 0; i < 16; i++)
-        pool.submit(print);
 
     return 0;
 }
